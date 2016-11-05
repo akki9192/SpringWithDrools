d8e0b6c88a80eb0cbc35ab816b4fe4778dce236d
akki9192 <akshaybhdr@gmail.com>
2016-11-05 03:36:25 -0700

    first commit

---
+++ springWithDrools/src/main/java/com/capgemini/model/RouteAttribute.java
@@ -0,0 +1,44 @@
+package com.capgemini.model;
+
+public class RouteAttribute {
+ 
+private String country;
+ private String state;
+ private boolean newEntry;
+  
+ //Result
+ private String ansOnRule;
+ 
+public String getCountry() {
+ return country;
+ }
+ 
+public void setCountry(String country) {
+ this.country = country;
+ }
+ 
+public String getState() {
+ return state;
+ }
+ 
+public void setState(String state) {
+ this.state = state;
+ }
+ 
+public boolean isNewEntry() {
+ return newEntry;
+ }
+ 
+public void setNewEntry(boolean newEntry) {
+ this.newEntry = newEntry;
+ }
+ 
+public String getAnsOnRule() {
+ return ansOnRule;
+ }
+ 
+public void setAnsOnRule(String ansOnRule) {
+ this.ansOnRule = ansOnRule;
+ }
+  
+}
